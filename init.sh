eaf74a89f7dd237c1cb09da656beb8449e9d72b6
zu-ann <asl29112@gmail.com>
2018-01-11 21:02:09 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,4 +8,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
-sudo /etc/init.d/gunicorn restart
+# sudo /etc/init.d/gunicorn restart
+sudo service gunicorn restart
