5e366e61831e080dc28149fbbd03f88c936f0a5a
zu-ann <asl29112@gmail.com>
2018-01-12 21:52:17 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,18 +4,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Question(models.Model):
-	title = models.CharField(max_length=255)
-	text = models.TextField()
-	added_at = models.DateField(blank=True)
-	rating = models.IntegerField()
-	author = models.ForeignKey(User)
-	likes = models.ManyToManyField(User)
+	title = models.CharField(max_length=255, default='')
+	text = models.TextField(default='')
+	added_at = models.DateField(blank=True, auto_now_add=True)
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+	likes = models.ManyToManyField(User, related_name='question_like_user')
 
 class Answer(models.Model):
-	text = models.TextField()
-	added_at = models.DateField(blank=True)
-	question = models.ForeignKey(Question)
-	author = models.ForeignKey(User)
+	text = models.TextField(default='')
+	added_at = models.DateField(blank=True, auto_now_add=True)
+	question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+	author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
 class QuestionManager(models.Manager):
 	def new(self):
