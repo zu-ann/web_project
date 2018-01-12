2811b58d735bc71b8ddfb010446e715c29d9b518
zu-ann <asl29112@gmail.com>
2018-01-12 16:10:09 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,9 @@
-# sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-# sudo /etc/init.d/nginx restart
-# sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-# sudo /etc/init.d/gunicorn restart
-# sudo /etc/init.d/mysql start
-
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf   /etc/gunicorn.d/test.conf
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/django.conf
 sudo /etc/init.d/gunicorn restart
-sudo service gunicorn restart
+# sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+# sudo service gunicorn restart
 
