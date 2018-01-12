ddc778a04c0eead09ebc617d9e62fec3588b6781
zu-ann <asl29112@gmail.com>
2018-01-12 16:39:36 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#6ahbm4q$wc#k8a$_4fg7sg&q5z759u$-meiiqke5erz-87p+a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
